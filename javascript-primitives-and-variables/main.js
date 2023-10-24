d843c49e8134b54be12f78e040b3a23406544060
moranalonso <146308490+moranalonso@users.noreply.github.com>
2023-10-24 00:44:14 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+const fullName = 'Alonso Moran';
+const isCool = true;
+const totalPets = 2;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+
+const unknown = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
